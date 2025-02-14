e559625bbdfcddc265ba5a527ca2082b5acb840e
adityarajverma <adityarajverma@gmail.com>
2025-02-14 23:29:30 +0530

    Update URI

--- src/main/java/com/ai/cricket/bot/controller/CricketBotController.java
+++ src/main/java/com/ai/cricket/bot/controller/CricketBotController.java
@@ -6,6 +6,7 @@ import com.ai.cricket.bot.service.ChatService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,8 @@ public class CricketBotController {
 
     @Autowired
     private ChatService chatService;
+
+    @GetMapping("/chat")
 
     public ResponseEntity getResponse(@RequestParam (name="inputText") String inputText) throws IOException {
         return  ResponseEntity.ok(chatService.getResponse(inputText));
