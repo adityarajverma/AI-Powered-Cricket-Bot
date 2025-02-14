b089a51b174d4058e12a033d8c528bf6c5623fac
adityarajverma <adityarajverma@gmail.com>
2025-02-14 23:02:13 +0530

    first commit

---
+++ src/main/java/com/ai/cricket/bot/AiPoweredCricketBotApplication.java
@@ -0,0 +1,13 @@
+package com.ai.cricket.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPoweredCricketBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiPoweredCricketBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai/cricket/bot/AiPoweredCricketBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.cricket.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPoweredCricketBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
